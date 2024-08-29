3640424306279c38576db05ec305dc49d49029b6
Nicodemus Karisa <nkarisa@ke.ci.org>
2024-08-29 07:24:26 +0300

    Initial - 4

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -4,7 +4,8 @@ if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
     
-    require_once('vendor/autoload.php');
+    // require_once 'vendor/autoload.php';
+    require_once  __DIR__ . '/vendor/autoload.php';
 
 
 // $dotenv = Dotenv\Dotenv::createImmutable(FCPATH);
