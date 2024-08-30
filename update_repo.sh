8fd67b42aa45c9372eeb277a370630f306eab6da
Nicodemus Karisa <nkarisa@ke.ci.org>
2024-08-30 10:02:11 +0300

    Added update repo sh file - Updated

--- update_repo.sh
+++ update_repo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Navigate to the Git repository
-cd /path/to/your/repository
+cd /home3/idxkyxmy/public_html/funds
 
 # Pull the latest changes from the main branch
 git pull origin main
