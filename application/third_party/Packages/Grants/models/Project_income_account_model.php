648f3d279ecfcfd5b6c0e67995a2e54f316ce27c
Nicodemus Karisa <nkarisa@ke.ci.org>
2024-09-14 12:44:06 +0300

    Update funder relationship

--- application/third_party/Packages/Grants/models/Project_income_account_model.php
+++ application/third_party/Packages/Grants/models/Project_income_account_model.php
@@ -87,12 +87,11 @@ class Project_income_account_model extends MY_Model{
         $lookup_values = parent::lookup_values();
 
         if(!$this->session->system_admin){
-            $this->read_db->join('funder','funder.funder_id=project.fk_funder_id');
-            $this->read_db->where(array('funder.fk_account_system_id'=>$this->session->user_account_system_id));
+            $this->read_db->where(['fk_funder_id' => hash_id($this->id, 'decode')]);
             $this->read_db->select(array('project_id','project_name'));
             $lookup_values['project'] = $this->read_db->get('project')->result_array();
 
-            $this->read_db->where(array('fk_account_system_id'=>$this->session->user_account_system_id,'income_account_is_active' => 1));
+            $this->read_db->where(['fk_funder_id' => hash_id($this->id, 'decode'), 'income_account_is_active' => 1]);
             $this->read_db->select(array('income_account_id','income_account_name'));
             $lookup_values['income_account'] = $this->read_db->get('income_account')->result_array();
         }
